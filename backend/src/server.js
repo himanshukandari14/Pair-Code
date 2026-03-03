6a8dd7298f4a3d1ccd23500776c311bfc7dc5400
himanshukandari14 <himanshukandari14@gmail.com>
2026-03-03 20:01:50 +0530

    new controllers

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,13 +5,14 @@ import { connectDB } from "./lib/db.js";
 import { serve } from "inngest/express"
 import { inngest, functions } from "./lib/inngest.js";
 import cors from "cors"
-
+import { clerkMiddleware } from "@clerk/express";
+import { protectRoute } from "./middleware/protectRoute.js";
+import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 
-const __dirname = path.resolve()
-
 app.use(express.json())
+app.use(clerkMiddleware())
 
 app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
 
@@ -21,9 +22,14 @@ const PORT=ENV.PORT;
 
 app.use("/api/inngest",serve({ client: inngest, functions}));
 
+app.use("/api/chat", chatRoutes)
 
 app.get("/",(req,res)=>{
     res.send("hellos");
+})
+
+app.get('/protected',protectRoute,(req,res)=>{
+    return res.send("message it is prtected")
 })
 
 app.listen(PORT,async()=>{
