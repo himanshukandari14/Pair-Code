8f1c7bcad5b06a7a974d779a912b4d296b1f6580
himanshukandari14 <himanshukandari14@gmail.com>
2026-03-03 17:25:51 +0530

    added inngest

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,24 @@
 import express from "express"
+import path from "path"
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
-
+import { serve } from "inngest/express"
+import { inngest } from "./lib/inngest.js";
 
 
 const app = express();
 
+const __dirname = path.resolve()
+
+app.use(express.json())
+
+app.use(cors({origin:ENV.CLIENT_URL,credentials:true}))
+
+
 const PORT=ENV.PORT;
+
+
+app.use("/api/inngest",serve({ client: inngest, functions}));
 
 
 app.get("/",(req,res)=>{
